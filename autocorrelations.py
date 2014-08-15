ef95986dca71b8f13d52bed483e2fe98a8239154
cstaines <cstaines419@hotmail.co.uk>
2014-08-15 13:54:56 +0100

    Added script to plot autocorrelations for 0 planets for all stars

--- autocorrelations.py
+++ autocorrelations.py
@@ -1,62 +1,23 @@
-#This program computes the autocorrelation of each parameter
-#in the .fits files for the 0 planet case,
-#to see if the MCMC chain samples can be treated as
-#independent
+#This function generates and plots the autocorrelation function
+#for each parameter, for each star, for the 0 planet MCMC.
 
-#Run this in the directory containing the .fits files
+#Use this in the 'GAIA-summer-project' directory.
 
+import pandas.tools.plotting as ptp
+import matplotlib.pyplot as plt
 import pyfits as pf
 import numpy as np
-import matplotlib.pyplot as plt
-from math import floor
-
-#Show figure on-screen if true; otherwise save as pdf (time-consuming)
-show = True
 
 for i in xrange(30):
+    
+    #Open the .fits file and store the data as a numpy record array
     hdus = pf.open('RECONS_0_planets/RECONS_gaia_test_{0}.fits'.format(i+1))
     names = hdus[1].columns.names
     cols = [hdus[1].data.field(col) for col in names]
     cat = np.rec.fromarrays(cols, names=names)
 
-    j = 0
-    autocorrs = []
-    
+    #Plot the autocorrelation of each column and save
     for col in names:
-        autocorrs.append(np.correlate(cat[col], cat[col], mode='full'))
-
-        if j % 4 == 0:
-            plt.figure(1+int(floor(j/4)))
-            plt.suptitle('Autocorrelations')
-            
-        plt.subplot(2, 2, 1+(j%4))
-        plt.plot(autocorrs[-1])
-        plt.xticks([0, 80000, 160000])
-        plt.title(col)
-
-        if j in [3, 7, 8]:
-            if show:
-                plt.show()
-            else:
-                plt.savefig('autocorrelations/star{0}/autocorrs_{1}.ps'.format(i+1, 1+int(floor(j/4))))
-        
-        j += 1
-
-
-            
-        
-        
-        
-
-
-        
-
-
-    
-    
-    
-    
-    
-    
-    
+        ptp.autocorrelation_plot(cat[col])
+        plt.savefig('autocorrelations/star{0}/autocorr_'.format(i+1)+col+'.pdf')
 
