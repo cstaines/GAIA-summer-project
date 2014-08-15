d7c17a62e9c82e6942f2f09dc6a682928d32ddf7
cstaines <cstaines419@hotmail.co.uk>
2014-08-15 09:34:59 +0100

    Added script to compute autocorrelations for parameters in zero-planet case

---
+++ autocorrelations.py
@@ -0,0 +1,55 @@
+#This program computes the autocorrelation of each parameter
+#in the .fits files for the 0 planet case
+#, to see if the MCMC chain samples can be treated as
+#independent
+
+#Run this in the directory containing the .fits files
+
+import pyfits as pf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_autos(inp_array, name, ind):
+    plt.subplot(2, 2, ind+1)
+    plt.plot(inp_array[name])
+    plt.title(name)
+    plt.show()
+    
+    
+autocorrs = []
+
+for i in xrange(30):
+    hdus = pf.open('RECONS_0_planets/RECONS_gaia_test_{0}'.format(i+1))
+    names = hdus[1].columns.names
+    cols = [hdus[1].data.field(col) for col in names]
+    cat = np.rec.fromarrays(cols, names=names)
+
+    for col, i in zip(names, range(len(names))):
+        autocorrs.append(np.correlate(cat[col], cat[col], mode='full'))
+        if i < 4:
+            plt.figure(0)
+            suptitle('Autocorrelations')
+            plot_autos(autocorrs, col, i)
+        else:
+            plt.figure(1)
+            suptitle('Autocorrelations')
+            plot_autos(autocorrs, col, i-4)
+
+
+            
+        
+        
+        
+
+
+        
+
+
+    
+    
+    
+    
+    
+    
+    
+
