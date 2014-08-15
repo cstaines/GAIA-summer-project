739824d2e2c5e86fbad0b1a8498b0dc24f149c35
cstaines <cstaines419@hotmail.co.uk>
2014-08-15 11:18:28 +0100

    Made showing plots on-screen optional

--- autocorrelations.py
+++ autocorrelations.py
@@ -1,6 +1,6 @@
 #This program computes the autocorrelation of each parameter
-#in the .fits files for the 0 planet case
-#, to see if the MCMC chain samples can be treated as
+#in the .fits files for the 0 planet case,
+#to see if the MCMC chain samples can be treated as
 #independent
 
 #Run this in the directory containing the .fits files
@@ -9,6 +9,9 @@ import pyfits as pf
 import numpy as np
 import matplotlib.pyplot as plt
 from math import floor
+
+#Show figure on-screen if true; otherwise save as pdf (time-consuming)
+show = True
 
 for i in xrange(30):
     hdus = pf.open('RECONS_0_planets/RECONS_gaia_test_{0}.fits'.format(i+1))
@@ -32,7 +35,10 @@ for i in xrange(30):
         plt.title(col)
 
         if (j+1) % 4 == 0:
-            plt.savefig('autocorrelations/star{0}/autocorrs_{1}.pdf'.format(i+1, 1+int(floor(j/4))))
+            if show:
+                plt.show()
+            else:
+                plt.savefig('autocorrelations/star{0}/autocorrs_{1}.ps'.format(i+1, 1+int(floor(j/4))))
         
         j += 1
 
